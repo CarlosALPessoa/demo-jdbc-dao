bbf1bfef7c0ef1b1edb635fdf2215aef682f0f77
CarlosAleandro <carlosaleanndro@gmail.com>
2023-11-26 18:47:20 -0300

    Retorno de uma pesquisa atráves do FindByID do SellerDao

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,22 +1,18 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDAO;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
-	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-		
+	public static void main(String[] args) {		
 		//Implementacao de interface sem conhecer a implementacao
 		SellerDAO sellerDAO = DaoFactory.createSellerDAO();
-		System.out.println(obj);
+		
+		//Buscando um vendedor por id
+		Seller seller = sellerDAO.findByID(3);
+		System.out.println(seller);
 	}
 
 }
