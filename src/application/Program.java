f518f885c3547cb667e5843d8d25fbb85710f6bf
CarlosAleandro <carlosaleanndro@gmail.com>
2023-11-27 09:38:58 -0300

    Atualização do Program e implementação enxuta do findById do SellerDao

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,6 +11,7 @@ public class Program {
 		SellerDAO sellerDAO = DaoFactory.createSellerDAO();
 		
 		//Buscando um vendedor por id
+		System.out.println("=== TEST 1: seller findById ==== ");
 		Seller seller = sellerDAO.findByID(3);
 		System.out.println(seller);
 	}
